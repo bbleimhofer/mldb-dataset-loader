0492543fc935c04cc7a74b5f33e256c42d05f5fa
Nicolas Kruchten <nicolas@datacratic.com>
2015-04-17 12:49:59 -0400

    initial commit

---
+++ doc/index.md
@@ -0,0 +1,5 @@
+## Dataset Loader Plugin Documentation
+
+This plugin allows you to load CSV files from the internet as datasets.
+
+[Click here to access the UI](../routes/static/index.html)
---
+++ main.py
@@ -0,0 +1,26 @@
+import json, csv, urllib, StringIO
+
+mldb.plugin.serve_static_folder('/static', 'static')
+mldb.plugin.serve_documentation_folder('doc')
+
+def request_handler(mldb, remaining, verb, resource, rest_params, payload,
+                    content_type, content_length, headers):
+    if verb == "POST" and remaining == "/loadcsv":
+        payload = json.loads(payload)
+        reader = csv.DictReader(open(urllib.urlretrieve(payload["url"])[0]))
+        dataset = mldb.create_dataset(dict(id=str(payload["name"]), type="mutable"))
+        for i, row in enumerate(reader):
+            values = []
+            row_name = i
+            for col in row:
+                if col == "":
+                    row_name = row[col]
+                else:
+                    values.append([col, row[col], 0])
+            dataset.record_row(row_name, values)
+        dataset.commit()
+        return "yay"
+    else:
+        raise Exception("404")
+
+mldb.plugin.set_request_handler(request_handler)
